9cce05d938ee141bf7b29449dba674f4bb2d6c68
mingzhuqiuxiahhq <hangqinghe@gmail.com>
2022-04-25 23:36:32 -0500

    updated request, read for 4 bike companies

--- main.py
+++ main.py
@@ -25,36 +25,43 @@ def request_file(url, zip_name):
     for p in period_range:
         p = p.strftime("%Y%m")
         file_url = url + p + zip_name
-        unzip_name = p + zip_name.split('.')[0] + '.csv'
+        if 'citibike' in zip_name.split('-'):
+            unzipped_file = 'JC-' + p + zip_name.split('.')[0] + '.csv'
+        else:
+            unzipped_file = p + zip_name.split('.')[0] + '.csv'
         # loop current directory to get file name
-        if exists(f'./{unzip_name}'):
-            print(f'{unzip_name} already exist')
-            file_list.append(unzip_name)
-            continue
+        if exists(f'./{unzipped_file}'):
+            print(f'{unzipped_file} already exist')
+            file_list.append(unzipped_file)
+            pass
         else:
             r = requests.get(file_url)
             zip_file = zipfile.ZipFile(BytesIO(r.content))
             zip_file.extractall('./')
-            file_list.append(unzip_name)
+            file_list.append(unzipped_file)
 
     return file_list
 
 
 def read_files(file_name, file_list):
     if file_name == 'capital bike':
-        usecols = [0, 1, 2, 3, 8, 9, 10, 11, 12]
+        usecolumns = [0, 1, 2, 3, 8, 9, 10, 11, 12]
     elif file_name == 'citi bike':
-        usecols = [0, 1, 2, 3, 8, 9, 10, 11, 12]
-    elif file_name == 'metro bike':
-        usecols = [0, 1, 2, 3, 5, 6, 8, 9, 10, 13, 14]
+        usecolumns = [0, 1, 2, 3, 8, 9, 10, 11, 12]
+    elif file_name == 'blue bike':
+        usecolumns = [0, 1, 2, 5, 6, 9, 10, 11, 12]
     elif file_name == 'divvy bike':
-        usecols = [0, 1, 2, 3, 8, 9, 10, 11, 12]
+        usecolumns = [0, 1, 2, 3, 8, 9, 10, 11, 12]
+
+
+
 
     if len(file_list) == 0:
         return
-    path = '../'
+
+    path = './'
     file_test = file_list[0]
-    df = pd.read_csv(path + file_test, header=0, usecols=usecols)
+    df = pd.read_csv(path + file_test, header=0, usecols=usecolumns)
     display(df.head(10))
 
     return df
@@ -73,4 +80,19 @@ if __name__ == '__main__':
     capital_bike_zip = '-capitalbikeshare-tripdata.zip'
     capital_bike_files = request_file(capital_bike_url, capital_bike_zip)
     capital_bike_df = read_files('capital bike', capital_bike_files)
+
+    citi_bike_url = 'https://s3.amazonaws.com/tripdata/JC-'
+    citi_bike_zip = '-citibike-tripdata.csv.zip'
+    citi_bike_files = request_file(citi_bike_url, citi_bike_zip)
+    citi_bike_df = read_files('citi bike', citi_bike_files)
+
+    divvy_bike_url = 'https://divvy-tripdata.s3.amazonaws.com/'
+    divvy_bike_zip = '-divvy-tripdata.zip'
+    divvy_bike_files = request_file(divvy_bike_url, divvy_bike_zip)
+    divvy_bike_df = read_files('divvy bike', divvy_bike_files)
+
+    blue_bike_url = 'https://s3.amazonaws.com/hubway-data/'
+    blue_bike_zip = '-bluebikes-tripdata.zip'
+    blue_bike_files = request_file(blue_bike_url, blue_bike_zip)
+    blue_bike_df = read_files('blue bike', blue_bike_files)
 
